3d4796afc208af4a252667d63c76ecb0b4d3725d
Simon Roche <rochesimon92@gmail.com>
2021-01-18 20:02:54 +0100

    Création de l'objet js

---
+++ js/main.js
@@ -0,0 +1,69 @@
+const enquete = [
+    {
+        title : "Le bar à chat",
+        indices : [
+            "Tornado adore se mettre en boule au niveau du dos des clients. Il n'est pas le chat préféré de l'ami qui a commandé un café.",
+            "Alan a reçu la visite d'un chat de plus que Lucie, et deux de moins que l'ami(e) qui a un faible pour Pitou",
+            "Le chapardeur est l'animal préféré de la personne qui a commandé un chocolat chaud. Elle a été approchée par un chat de plus que celle qui a craqué pour le félin s'installant sur les genoux des clients",
+            "L'ami qui a pris un thé a fondu pour Chataîgne",
+            "Matt n'a pas commandé de café",
+        ],
+        elements : [
+            {
+                name : 'Amis',
+                list : ['Alan', 'Isabelle', 'Lucie', 'Matt']
+            },
+            {
+                name : 'Boisson',
+                list : ['Café', 'Chocolat', 'Limonade', 'Thé']
+            },
+            {
+                name : 'Félins',
+                list : ['3', '4', '5', '6']
+            },
+            {
+                name : 'Chat',
+                list : ['Châtaigne', 'Pitou', 'Quizzy', 'Tornado']
+            },
+            {
+                name : 'Paticularité',
+                list : ['Caresses', 'Chapardeur', 'Dos', 'Genoux']
+            },
+        ],
+        goodAns : [
+            "A1","B4","C3","D2","E4","F3","G2","H1","I3","J2","K4","L1","M1","N2","O3","P4"
+        ]
+    },
+
+    {
+        title : "La main dans le sac",
+        indices : [
+            "Le pop-corn sucré a été commandé en même temps que les biscuits, mais pas par Océane.",
+            "Le paquet d'Alexis contenait cent grammes de plus que celui de Mathilde, et cinquante de moins que celui au beurre.",
+            "Le pop-corn enrobé de chocolat n'a pas été acheté par Alexis ni par celui/celle qui a pris des barres chocolatés.",
+            "Eva a commandé de l'eau avec son pop-corn salé ou sucré. Son paquet contenait cinquante grammes de moins que celui de l'ami(e) qui a commandé un soda.",
+            "Océane a commandé 200g de pop-corn",
+        ],
+        elements : [
+            {
+                name : 'Copain',
+                list : ['Alexis', 'Eva', 'Mathilde', 'Océane', 'Théo']
+            },
+            {
+                name : 'Pop-corn',
+                list : ['Beurre', 'Caramel', 'Chocolat', 'Salé', "Sucré"]
+            },
+            {
+                name : 'Quantité',
+                list : ['200g', '250g', '300g', '350g', '400g']
+            },
+            {
+                name : 'Achat',
+                list : ['Biscuit', 'Bonbons', 'Chocolat', 'Eau', 'Soda']
+            },
+        ],
+        goodAns : [
+            "A1","B4","C3","D2","E4","F3","G2","H1","I3","J2","K4","L1","M1","N2","O3","P4"
+        ]
+    },
+]
