341d754f39d1f4f67394006b32ac4ee13a9ca94b
Simon Roche <rochesimon92@gmail.com>
2021-01-22 13:05:41 +0100

    Ajout du wireframe

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const enquete = [
     {
         title : "Le bar à chat",
+        largeur : 4,
+        longueur : 4,
         indices : [
             "Tornado adore se mettre en boule au niveau du dos des clients. Il n'est pas le chat préféré de l'ami qui a commandé un café.",
             "Alan a reçu la visite d'un chat de plus que Lucie, et deux de moins que l'ami(e) qui a un faible pour Pitou",
@@ -9,10 +11,6 @@ const enquete = [
             "Matt n'a pas commandé de café",
         ],
         elements : [
-            {
-                name : 'Amis',
-                list : ['Alan', 'Isabelle', 'Lucie', 'Matt']
-            },
             {
                 name : 'Boisson',
                 list : ['Café', 'Chocolat', 'Limonade', 'Thé']
@@ -29,6 +27,10 @@ const enquete = [
                 name : 'Paticularité',
                 list : ['Caresses', 'Chapardeur', 'Dos', 'Genoux']
             },
+            {
+                name : 'Amis',
+                list : ['Alan', 'Isabelle', 'Lucie', 'Matt']
+            }
         ],
         goodAns : [
             "A1","B4","C3","D2","E4","F3","G2","H1","I3","J2","K4","L1","M1","N2","O3","P4"
@@ -63,7 +65,59 @@ const enquete = [
             },
         ],
         goodAns : [
-            "A1","B4","C3","D2","E4","F3","G2","H1","I3","J2","K4","L1","M1","N2","O3","P4"
+            "A5","B1","C4","D2","E3","F4","G3","H2","I1","J5","K3","L4","M5","N2","O1"
         ]
     },
 ]
+
+$('#salut').append('<h1>' + enquete[0].title + "</h1>")
+$('#salut')
+
+for (let index = 0; index < enquete[0].indices.length; index++) {
+    $('#salut').append('<p>' + (index+1) +'. '+enquete[0].indices[index] + "</p><br>")   
+}
+
+for (let index = 0; index < enquete[0].elements[0].list.length; index++) {
+    $('#ligne1').append('<p class="rotate">' + enquete[0].elements[0].list[index] + '</p>')
+}
+
+for (let index = 0; index < enquete[0].elements[1].list.length; index++) {
+    $('#ligne2').append('<p class="rotate">' + enquete[0].elements[1].list[index] + '</p>')
+}
+
+for (let index = 0; index < enquete[0].elements[2].list.length; index++) {
+    $('#ligne3').append('<p class="rotate">' + enquete[0].elements[2].list[index] + '</p>')
+}
+
+for (let index = 0; index < enquete[0].elements[3].list.length; index++) {
+    $('#ligne4').append('<p class="rotate">' + enquete[0].elements[3].list[index] + '</p>')
+}
+
+for (let index = 0; index < enquete[0].elements[4].list.length; index++) {
+    $('#ligne5').append('<p>' + enquete[0].elements[4].list[index] + '</p>')
+}
+
+for (let index = 0; index < enquete[0].elements[3].list.length; index++) {
+    $('#ligne6').append('<p>' + enquete[0].elements[3].list[index] + '</p>')
+}
+
+for (let index = 0; index < enquete[0].elements[2].list.length; index++) {
+    $('#ligne7').append('<p>' + enquete[0].elements[2].list[index] + '</p>')
+}
+
+for (let index = 0; index < enquete[0].elements[1].list.length; index++) {
+    $('#ligne8').append('<p>' + enquete[0].elements[1].list[index] + '</p>')
+}
+
+$('.petitCarre').click(function() {
+    $(this).removeClass('petitCarre'),
+    $(this).addClass('petitCarreNon')
+    $('.petitCarreNon').click(function() {
+        $(this).removeClass('petitCarreNon'),
+        $(this).addClass('petitCarreOui')
+        $('.petitCarreOui').click(function() {
+            $(this).removeClass('petitCarreOui'),
+            $(this).addClass('petitCarre')
+        })
+    })   
+})
